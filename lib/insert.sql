8453776f50f60887fc9da0e2693d3b9016bd8518
Richard Scott Salchunas <sccrlx4ss2@yahoo.com>
2018-11-27 19:58:19 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
--- INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (1, "Mr. Chocolate", 99, "M", "Dark", "Smooth" 0);
--- INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (2, "Rowdy", 21, "M", "Red", "Somewhat Rowdy", 1);
--- INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (3, "Tabitha", 48, "F", "Ecru", "Dismissive", 1);
--- INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (4, "Sergeant Brown", 41, "M", "Red, White and Blue", "Well-groomed", 0);
--- INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (5, "Melissa", 32, "F", "Blood red", "Explosive", 1);
--- INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (6, "Grinch", 112, "M", "Green right down to the core", "Uncouth", 1);
--- INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (7, "Wendy", 22, "F", "Red dress", "Inviting", 0);
--- INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (8, null, 9, "M", "Distinguished grey", "Grizzly", 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (1, "Mr. Chocolate", 99, "M", "Dark", "Smooth" 0);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (2, "Rowdy", 21, "M", "Red", "Somewhat Rowdy", 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (3, "Tabitha", 48, "F", "Ecru", "Dismissive", 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (4, "Sergeant Brown", 41, "M", "Red, White and Blue", "Well-groomed", 0);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (5, "Melissa", 32, "F", "Blood red", "Explosive", 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (6, "Grinch", 112, "M", "Green right down to the core", "Uncouth", 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (7, "Wendy", 22, "F", "Red dress", "Inviting", 0);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (8, null, 9, "M", "Distinguished grey", "Grizzly", 1);
